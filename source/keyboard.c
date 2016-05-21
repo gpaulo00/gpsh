716749c53fd9ac9f6d27f9a24cf19f640f122364
gpaulo00 <gustavo.paulo.segura@gmail.com>
2016-05-21 01:40:52 -0400

    Deleted Yen/Yuan and Eur signs

--- source/keyboard.c
+++ source/keyboard.c
@@ -1,7 +1,7 @@
 #include <main.h>
 
-//Arreglar punto medio, cierra corchete, libra esterlina, yen, tilde, euro, \
-// Testing yen, \, punto medio, cierra corchete, tilde, libra, euro
+//Arreglar yen, tilde, euro
+// Testing yen,  tilde, euro
 
 void keyboard(touchPosition touch) {
     if (touchInBox(touch, 0,120, 31,30)){printf_kb("qQ11");} //Letras
@@ -17,25 +17,29 @@ void keyboard(touchPosition touch) {
     
     else if (touchInBox(touch, 0,151, 31,30)){printf_kb("aA@^");}
     else if(touchInBox(touch, 32,151, 31,30)){printf_kb("sS#[");}
-    else if(touchInBox(touch, 64,151, 31,30)){printf_kb("dD\x80\x5d");} //Testing
+    else if(touchInBox(touch, 64,151, 31,30)){printf_kb("dD/\x5d");} //Testing (euro 3rd)
     else if(touchInBox(touch, 96,151, 31,30)){printf_kb("fF%{");}
     else if(touchInBox(touch, 128,151, 31,30)){printf_kb("gG&}");}
     else if(touchInBox(touch, 160,151, 31,30)){printf_kb("hH*<");}
     else if(touchInBox(touch, 192,151, 31,30)){printf_kb("jJ(>");}
     else if(touchInBox(touch, 224,151, 31,30)){printf_kb("kK)$");}
-    else if(touchInBox(touch, 256,151, 31,30)){printf_kb("lL-\x9c");} //Testing
-    else if(touchInBox(touch, 288,151, 31,30)){printf_kb("\xa4\xa5\x5c\xbe");} //Testing
+    else if(touchInBox(touch, 256,151, 31,30)){printf_kb("lL-\x9c");}
+    else if(touchInBox(touch, 288,151, 31,30)){printf_kb("\xa4\xa5\x5c|");} //Testing (only yen, 4rd)
     
     else if (touchInBox(touch, 0,182, 31,30)){ shift(); }
     else if (touchInBox(touch, 0,213, 62,30)){ abc(); }
     
-    else if(touchInBox(touch, 32,182, 31,30)){printf_kb("zZ!\xfa");} //Testing
+    else if(touchInBox(touch, 32,182, 31,30)){printf_kb("zZ!\xfa");}
     else if(touchInBox(touch, 64,182, 31,30)){printf_kb("xX;-");}
     else if(touchInBox(touch, 96,182, 31,30)){printf_kb("cC:+");}
     else if(touchInBox(touch, 128,182, 31,30)){printf_kb("vV'=");}
     else if(touchInBox(touch, 160,182, 31,30)){printf_kb("bB\"_");}
-    else if(touchInBox(touch, 192,182, 31,30)){printf_kb("nN?\x7e");} //Testing
-    else if(touchInBox(touch, 224,182, 31,30)){printf_kb("mM/|");}
+    else if(touchInBox(touch, 192,182, 31,30)){printf_kb("nN?~");} //Testing
+
+    else if(touchInBox(touch, 224,182, 31,30)){ // m, M or Backspace
+        if(teclado==0||teclado==1){ printf_kb("mM  "); }
+        else { backspace(); }
+    }
 
     else if(touchInBox(touch, 256,182, 62,30)){ backspace(); } //Back
 
